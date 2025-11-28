b458b153f3d82f441484506502eacbcbf008f56c
Fathima Naila <fathimanaila71@gmail.com>
2025-11-28 22:38:15 +0530

    set the view function of register

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,5 +3,7 @@ from django.urls import path,include
 from accounts import views
 
 urlpatterns = [
-    path('',views.login,name='login')
+    path('',views.welcome,name='welcome'),
+    path('register/',views.register,name='register'),
+    path('login/',views.login,name='login')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,30 @@
-from django.shortcuts import render
+    # myapp/views.py
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import logout as auth_logout
 
-# Create your views here.
+from django.contrib.auth import authenticate, login
+
+def welcome(request):
+    return render(request,'welcome.html')
 def login(request):
     return render(request,'login.html')
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+        
+        myuser = User.objects.create_user(username,email,password)
+        myuser.fname = first_name
+        myuser.lname = last_name
+        myuser.save()
+        messages.success(request,"Your account has been successfully created")
+        return redirect('login')
+    return render(request,'login.html')
+def logout(request):
+    auth_logout(request)
