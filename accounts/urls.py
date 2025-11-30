66f3166cb66030863c220f5ffb9db18915de2e44
Fathima Naila <fathimanaila71@gmail.com>
2025-11-30 09:24:48 +0530

    set the logout view

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,6 @@ from accounts import views
 urlpatterns = [
     path('',views.welcome,name='welcome'),
     path('register/',views.register,name='register'),
-    path('login/',views.login,name='login')
+    path('login/',views.login_view,name='login'),
+    path('logout/',views.logout_view,name='logout')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -7,9 +7,22 @@ from django.contrib.auth import logout as auth_logout
 from django.contrib.auth import authenticate, login
 
 def welcome(request):
-    return render(request,'welcome.html')
-def login(request):
-    return render(request,'login.html')
+    return render(request,'accounts/welcome.html')
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user = authenticate(username=username,password=password)
+        
+        if user is not None:
+            login(request,user)
+            first_name= user.first_name
+            return render(request,'accounts/index.html',{'first_name':first_name})
+        else:
+            messages.error(request,"invalid credentials")
+            return redirect(welcome)
+    return render(request,'accounts/login.html')
 def register(request):
     if request.method == 'POST':
         username = request.POST['username']
@@ -19,12 +32,13 @@ def register(request):
         password = request.POST['password']
         confirm_password = request.POST['confirm_password']
         
-        myuser = User.objects.create_user(username,email,password)
+        myuser = User.objects.create_user(username,email,password,confirm_password)
         myuser.fname = first_name
         myuser.lname = last_name
         myuser.save()
         messages.success(request,"Your account has been successfully created")
         return redirect('login')
-    return render(request,'login.html')
-def logout(request):
+    return render(request,'accounts/register.html')
+def logout_view(request):
     auth_logout(request)
+    return redirect('login')
